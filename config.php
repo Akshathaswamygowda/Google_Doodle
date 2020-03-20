e9a69cdf6b735141a73664f33452086f85d4c101
Akshatha Thonnuru Swamygowda <42483354+Akshathaswamygowda@users.noreply.github.com>
2020-03-19 17:59:37 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 ob_start(); //output buffering
 try{
-	$con = new PDO("mysql:dbname=googledoodle;host=localhost", "root", "");
+	$con = new PDO("mysql:dbname=heroku_6d71e0a64f79d2b;host=us-cdbr-iron-east-04.cleardb.net", "b7d085ca5a1e5d", "2a0446ac");
 	$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
 }
